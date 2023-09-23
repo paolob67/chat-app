515deb5181765180cdcd2aad074a1290c66fea2d
Paolo Bianchini <5672449+paolob67@users.noreply.github.com>
2023-09-23 10:40:25 +0200

    create

---
+++ chat-app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title('Watsonx Chatbot 🤖')
+
+with st.sidebar:
+    watsonx_api_key = st.text_input("Watsonx API Key", key="watsonx_api_key", type="password")
+    watsonx_url = st.text_input("Watsonx URL", key="watsonx_url", value="https://us-south.ml.cloud.ibm.com", type="default")   
+    #TODO: change this to a select box with more than one model
+    watsonx_model = st.text_input("Model", key="watsonx_model", value="meta-llama/llama-2-70b-chat", type="default")   
+
+if 'messages' not in st.session_state: 
+    st.session_state.messages = [{"role": "assistant", "content": "How can I help you?"}] 
+
+for message in st.session_state.messages: 
+    st.chat_message(message['role']).markdown(message['content'])
+
+prompt = st.chat_input('Pass Your Prompt here')
+
+if prompt: 
+    st.chat_message('user').markdown(prompt)
+    st.session_state.messages.append({'role':'user', 'content':prompt})
+
+    response = "You said: " + prompt
+    
+    st.chat_message('assistant').markdown(response)
+    st.session_state.messages.append({'role':'assistant', 'content':response})
